73dbb15a0a468abd40a8cf3ffd45d56f6b4505f3
yanis demdoum <doumzir@DESKTOP-SQU8VAT.localdomain>
2022-10-28 09:02:43 +0200

    ajout des catégorie et debut de la galerie

---
+++ index.js
@@ -0,0 +1,34 @@
+
+class Article {
+    constructor(jsonArticle) {
+        jsonArticle && Object.assign(this, jsonArticle);
+    }
+
+
+}
+class Categorie {
+    constructor(jsonCategorie) {
+        jsonCategorie && Object.assign(this, jsonCategorie);
+    }
+
+
+}
+
+
+fetch('http://localhost:5678/api/categories').then(categoriedata => categoriedata.json()).then(jsonListCategorie => {
+    for (let jsonCategorie of jsonListCategorie) {
+        let categorie = new Categorie(jsonCategorie);
+        document.querySelector(".filter").innerHTML += `<li class="filterLi" id="categorie${categorie.id}">${categorie.name}</li>`
+    }
+});
+console.log("salut");
+fetch('http://localhost:5678/api/works').then(dataarticle => dataarticle.json()).then(jsonListArticle => {
+    for (let jsonArticle of jsonListArticle) {
+        let article = new Article(jsonArticle);
+        document.querySelector(".gallery").innerHTML += `<figure id="article${article.id}">
+					<img src="${article.imageUrl}" alt="${article.title}">
+					
+					<figcaption>${article.title}</figcaption>
+				</figure>`
+    }
+})
